326cda3750ff0a439f2367f25f841ef155a2ab1c
Trashcode <86529713+Larrylee0201@users.noreply.github.com>
2024-05-01 23:55:34 -0400

    Update Quadratic equation calculator.cpp

--- Quadratic equation calculator.cpp
+++ Quadratic equation calculator.cpp
@@ -5,6 +5,7 @@
 
 #include <iostream>
 #include <cmath>
+
 using namespace std;
 
 //Function prototypes
@@ -12,23 +13,34 @@ double a;
 double b;
 double c;
 
-//Function to solve the quadratic equation
+//Function to solve the quadratic equation answers
 double x1;
 double x2;
 
+//funtion to solve the quadratic equation vertex
+double h;
+double k;
+
+//function to solve the quadratic equation axis of symmetry
 void solve(double &x1, double &x2);
+//function to solve the quadratic equation vertex
+void vertex(double &h, double &k);
 
 int main() {
-	
-  cout << "Please enter the value of a: ";
-  cin >> a;
-  cout << "Please enter the value of b: ";
-  cin >> b;
-  cout << "Please enter the value of c: ";
-  cin >> c;
+	cout << "The format will be like ax^2 + bx + c = 0" << endl;
 
-  solve(x1, x2); //call function solve()
-  cout << "\nThe value of x1 is: " << x1 << endl << "The value of x2 is : " << x2 << endl;
+	cout << "\nPlease enter the value of a: ";
+	cin >> a;
+	cout << "Please enter the value of b: ";
+	cin >> b;
+	cout << "Please enter the value of c: ";
+	cin >> c;
+
+	solve(x1, x2); //call function solve()
+	cout << "\nThe value of x1 is: " << x1 << endl << "The value of x2 is : " << x2 << endl;
+	
+  	vertex(h, k);//call function vertex()
+	cout << "\nThe vertex of h is: " << h << endl << "The value of k is : " << k << endl;
 	
 }
 
@@ -36,5 +48,10 @@ int main() {
 void solve(double &x1, double &x2){
 	x1 = (-b+sqrt(b*b-4*a*c))/(2*a);
 	x2 = (-b-sqrt(b*b-4*a*c))/(2*a);
-	
+}
+
+//function vertex()
+void vertex(double &h, double &k){
+	h = -b/(2*a);
+	k = a*h*h+b*h+c;
 }
